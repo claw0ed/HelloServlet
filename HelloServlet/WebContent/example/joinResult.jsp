608b76f5344c5926127e97b527a3af5dcd65cabf
claw0ed <claw0ed@gmail.com>
2018-01-18 18:45:33 +0900

    2018.01.18

---
+++ HelloServlet/WebContent/example/joinResult.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta charset="UTF-8">
+<title>회원가입</title>
+</head>
+<body>
+<h3>회원가입완료</h3>
+
+	ID : ${ member.id }<br>
+	이름 : ${ member.name }<br>
+	직업 : ${ member.job }<br>
+	이메일 : ${ member.email }<br>
+
+</body>
+</html>
---
+++ HelloServlet/src/claw0ed/JoinServlet.java
@@ -0,0 +1,45 @@
+package claw0ed;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import claw0ed.model.Member;
+
+/**
+ * Servlet implementation class JoinServlet
+ */
+@WebServlet("/JoinServlet.do")
+public class JoinServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest req, HttpServletResponse res)
+	 */
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		
+		String id = req.getParameter("id");
+		String name = req.getParameter("name");
+		String job = req.getParameter("job");
+		String email = req.getParameter("email");
+		
+		Member member = new Member();
+		member.setId(id);
+		member.setName(name);
+		member.setJob(job);
+		member.setEmail(email);
+		
+		req.setAttribute("member", member);
+		RequestDispatcher view = req.getRequestDispatcher("/example/joinResult.jsp");
+		view.forward(req, res);
+		
+	}
+
+}
